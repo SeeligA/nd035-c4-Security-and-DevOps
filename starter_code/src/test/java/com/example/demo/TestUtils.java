00451c1d089ea260532473af75def41cfb0051dc
SeeligA <as@arndtseelig.com>
2021-03-13 17:01:12 +0100

    Fix Java 9 Maven build issue

--- starter_code/src/test/java/com/example/demo/TestUtils.java
+++ starter_code/src/test/java/com/example/demo/TestUtils.java
@@ -10,10 +10,15 @@ public class TestUtils {
         boolean wasPrivate = false;
         try {
             Field f = target.getClass().getDeclaredField(fieldName);
-            wasPrivate = !f.isAccessible();
-            f.trySetAccessible();
+            if (!f.isAccessible()) {
+                f.setAccessible(true);
+                wasPrivate = true;
+            }
             f.set(target, toInject);
-            f.setAccessible(wasPrivate);
+            if (wasPrivate) {
+                f.setAccessible(false);
+            }
+
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
